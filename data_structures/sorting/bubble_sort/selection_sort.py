2d4bc97ba859002057c4d8d255bd634388f6e8dc
imran-tns <imran.eee.cuet@gmail.com>
2024-05-31 17:12:01 +0600

    selection sort refactored

--- data_structures/sorting/bubble_sort/selection_sort.py
+++ data_structures/sorting/bubble_sort/selection_sort.py
@@ -1,27 +1,14 @@
-# def selection_sort(arr):
-#     # Traverse through all array elements
-#     for i in range(len(arr)):
-#         # Find the minimum element in remaining unsorted array
-#         min_idx = i
-#         for j in range(i+1, len(arr)):
-#             if arr[min_idx] > arr[j]:
-#                 min_idx = j
-        
-#         # Swap the found minimum element with the first element
-#         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-#     return arr
-
-def selection_sort(arr):
+def selection_sort(arr_to_sort):
     """Sorts an array using the selection sort algorithm."""
-    for i in range(len(arr)):
+    for i in range(len(arr_to_sort)):
         # Find the minimum element in the unsorted part of the array
-        min_idx = find_min_index(arr, i)
-        
-        # Swap the minimum element with the current element
-        swap(arr, i, min_idx)
+        min_idx = find_min_index(arr_to_sort, i)
 
-    return arr
+        # Swap the minimum element with the current element
+        swap(arr_to_sort, i, min_idx)
+
+    return arr_to_sort
+
 
 def find_min_index(arr, start_index):
     """Returns the index of the minimum element in the unsorted part of the array."""
@@ -31,13 +18,13 @@ def find_min_index(arr, start_index):
             min_idx = j
     return min_idx
 
-def swap(arr, i, j):
+
+def swap(arr, left_index, right_index):
     """Swaps the elements at indices i and j in the array."""
-    arr[i], arr[j] = arr[j], arr[i]
+    arr[left_index], arr[right_index] = arr[right_index], arr[left_index]
+
 
 # Example usage:
 arr = [64, 25, 12, 22, 11]
 sorted_arr = selection_sort(arr)
 print("Sorted array:", sorted_arr)
-
-
