9996fb0c9454e549f741d038f80e130c3af89ff3
imran-tns <imran.eee.cuet@gmail.com>
2024-06-04 14:50:20 +0600

    trapping rain water solution fixed

--- leetcode/trapping_rain_water.py
+++ leetcode/trapping_rain_water.py
@@ -6,24 +6,16 @@ class Solution:
         left = 0
         right = 0
         total_area = 0
-
-        while left < len(height) - 1:
-            if height[left]:
-                right += 1
-                while height[left] > height[right]:
-                    right += 1
-                    if right == len(height):
-                        return total_area
-                    if right+1 < len(height):
-                        if height[right + 1] < height[right] > height[right - 1]:
-                            break
-
-                min_height = min(height[left], height[right])
-                left += 1
-                while left < right:
-                    total_area += min_height - height[left]
-                    left += 1
-            else:
-                left += 1
-                right += 1
+        max_left = []
+        max_right = []
+        for i in range(len(height)):
+            if height[i] > left:
+                left = height[i]
+            max_left.append(left)
+            if height[len(height) - 1 - i] > right:
+                right = height[len(height) - 1 - i]
+            max_right.append(right)
+        max_right.reverse()
+        for j in range(len(height)):
+            total_area += min(max_left[j], max_right[j]) - height[j]
         return total_area
